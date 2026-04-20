2e663ab147924b4bbeeb6846af1fbfda72838b6a
Divyansh Yadav <ydivyansh2789@gmail.com>
2026-04-20 06:58:51 +0530

    model evaluation and pusher added

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -6,6 +6,8 @@ class DataIngestionArtifact:
     trained_file_path:str 
     test_file_path:str 
     
+    
+    
 @dataclass
 class DataValidationArtifact:
     validation_status:bool
@@ -13,11 +15,13 @@ class DataValidationArtifact:
     drift_report_file_path: str
     
     
+    
 @dataclass
 class DataTransformationArtifact:
     transformed_object_file_path:str 
     transformed_train_file_path:str
     transformed_test_file_path:str
+    
 
 
 @dataclass
@@ -25,6 +29,7 @@ class ClassificationMetricArtifact:
     f1_score:float
     precision_score:float
     recall_score:float
+    
 
 
 
@@ -32,3 +37,21 @@ class ClassificationMetricArtifact:
 class ModelTrainerArtifact:
     trained_model_file_path:str 
     metric_artifact:ClassificationMetricArtifact
+    
+    
+    
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
+    
+    
+    
+    
+@dataclass
+class ModelPusherArtifact:
+    bucket_name:str
+    s3_model_path:str
