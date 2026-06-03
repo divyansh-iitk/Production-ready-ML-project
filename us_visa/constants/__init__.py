940228e93fe44514a327ce848626cda9d27ce669
Divyansh Yadav <ydivyansh2789@gmail.com>
2026-06-03 08:08:42 +0530

    updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -25,7 +25,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
-REGION_NAME = "ap-southeast-2"
+REGION_NAME = "ap-south-1"
 
 
 
